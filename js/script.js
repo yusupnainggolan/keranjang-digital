66454d40883eb27b1e4c7cd2a0427fe38244335b
Yusup Wandi Marlen <yusupnainggolan1@gmail.com>
2023-12-11 12:02:12 +0700

    menambah search bar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Toggle class active
+//Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
@@ -6,11 +6,26 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik diluar sidebar untuk menghilangkan nav
+//Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik diluar elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const searchbutton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!searchbutton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
